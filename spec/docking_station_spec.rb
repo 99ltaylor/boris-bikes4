7d207389c1990f84c94e41824417f696c11fc259
Luke Taylor <99ltaylor@gmail.com>
2021-08-26 17:56:50 +0200

    added comments for additional challenge 11 test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,15 @@
-require 'docking_station.rb'
+require "docking_station.rb"
 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
+  it "releases working bikes" do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  # needs more work challenge 11
+  # it 'Should dock bike in the docking station' do
+  #   bike = subject.dock_bike(hired)
+  #   expect(bike).to
 end
